2973efe31fcf2602a904b931e30da288c9f80f99
ww24 <ww24gm@gmail.com>
2013-04-28 00:25:49 +0900

    add members page of each team

--- controllers/pages.js
+++ controllers/pages.js
@@ -2,6 +2,16 @@
  * Pages Controller
  */
 
+var fs = require("fs"),
+    path = require("path");
+
+var ignoreList = ["auth", "logout"];
+fs.readdir(path.join(__dirname, "../static"), function (err, files) {
+  if (err)
+    throw err;
+  [].push.apply(ignoreList, files);
+});
+
 module.exports = function (context) {
   var app = context.app,
       model = context.model,
@@ -73,10 +83,11 @@ module.exports = function (context) {
   });
 
   // check logged in
-  app.get("/:page", function (req, res, next) {
-    if (!~ ["auth", "logout"].indexOf(req.params.page))
+  app.get("/:page*", function (req, res, next) {
+    if (!~ ignoreList.indexOf(req.params.page))
       if (! req.user) {
-        req.flash("redirect", req.params.page);
+        req.flash("redirect");
+        req.flash("redirect", req.originalUrl);
         return res.redirect("/auth");
       } else {
         model.User.getProfileList(req.user.id, function (err, user_profiles) {
@@ -102,13 +113,21 @@ module.exports = function (context) {
       title: "Members",
       template: "members",
       teams: [],
-      members: []
+      members: [],
+      team: "All"
     });
 
     model.User.getProfileList(function (err, users) {
       if (err)
         console.log(err);
 
+      users = users.map(function (user) {
+        return {
+          name: user.name,
+          team: user.team,
+          role: user.role
+        };
+      });
       res.locals.members = users;
 
       model.Team.getNameList(function (err, teams) {
@@ -118,6 +137,57 @@ module.exports = function (context) {
         res.locals.teams = teams;
         res.render(res.locals.template);
       });
+    });
+  });
+  app.get("/members/:team", function (req, res) {
+    res.locals({
+      root: res.locals,
+      error: req.flash("error"),
+      profile: null,
+      title: "Members",
+      template: "members",
+      teams: [],
+      members: [],
+      team: req.params.team
+    });
+
+    model.Team.find(function (err, teams) {
+      if (err)
+        console.log(err);
+
+      for (var i = 0, l = teams.length; i < l; i++)
+        if (req.params.team === teams[i].name)
+          break;
+
+      if (i === l)
+        return res.json(404, {error: "page not found"});
+
+      res.locals.teams = teams.map(function (teams) {
+        return teams.name;
+      }).sort();
+
+      model.User
+        .find({team: teams[i]._id})
+        .populate("team")
+        .populate("role")
+        .exec(function (err, users) {
+          if (err)
+            console.log(err);
+
+          res.locals.members = users.map(function (user) {
+            return {
+              name: user.name,
+              team: user.team.map(function (team) {
+                return team.name;
+              }).sort(),
+              role: {
+                name: user.role.name,
+                permission: user.role.permission
+              }
+            };
+          });
+          res.render(res.locals.template);
+        });
     });
   });
 
