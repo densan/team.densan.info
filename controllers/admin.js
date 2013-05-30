e72d35d7c1e704632bb5bca802ad0434ecfed1d8
ww24 <ww24gm@gmail.com>
2013-05-31 00:26:33 +0900

    add userlist.text getter #9

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,6 +2,10 @@
  * New Controller
  */
 
+var hogan = require("hogan.js"),
+    path = require("path"),
+    fs = require("fs");
+
 module.exports = function (context) {
   var app = context.app,
       router = context.router,
@@ -13,14 +17,45 @@ module.exports = function (context) {
       root: res.locals,
       error: req.flash("error"),
       title: "Admin",
-      template: "admin",
-      members: []
+      template: "admin"
     });
 
     // check permission
-    if (req.user.role)
-      console.log(req.user.role.permission);
+    if (!~ [].indexOf.call(req.user.role, "control-panel"))
+      return res.json(400, {message: "Bad Request"});
 
     res.render(res.locals.template);
+  });
+
+  router.get(2, "/admin/:pass/userlist.txt", function (req, res) {
+    res.locals({
+      root: res.locals,
+      error: req.flash("error"),
+      title: "Role - Admin",
+      template: "adminGetUserList.txt"
+    });
+
+    if (req.params.pass !== process.env.MAINTENANCE_PASS)
+      return res.json(400, {message: "Bad Request"});
+
+    // get user list
+    model.User.getProfileList(function (err, users) {
+      if (err)
+        console.log(err);
+
+      console.log("\033[31mAdminAccess: \033[32m%s\033[m", req.user.id);
+
+      res.locals.members = users;
+      res.set({
+        "Content-Type": "text/csv"
+      });
+
+      fs.readFile(path.join(app.get("views"), res.locals.template), "utf8", function (err, data) {
+        if (err)
+          throw err;
+        var userlist = hogan.compile(data).render({members: users});
+        res.send(userlist);
+      });
+    });
   });
 };
