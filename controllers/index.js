7b01e0b2f1f6941c3e4075239100ecbbef3f25f4
ww24 <ww24gm@gmail.com>
2014-04-10 01:09:34 +0900

    fix route bug

--- controllers/index.js
+++ controllers/index.js
@@ -26,26 +26,27 @@ module.exports = function (app, passport) {
 
   model = model(url, Validator);
 
-  var routes = [],
-      router = {
-        get: proxy.bind(app, "get"),
-        post: proxy.bind(app, "post"),
-        put: proxy.bind(app, "put"),
-        head: proxy.bind(app, "head"),
-        "delete": proxy.bind(app, "delete"),
-        options: proxy.bind(app, "options"),
-        trace: proxy.bind(app, "trace")
-      };
+  var routes = [];
+  var router = {
+    all: proxy.bind(app, "all"),
+    get: proxy.bind(app, "get"),
+    post: proxy.bind(app, "post"),
+    put: proxy.bind(app, "put"),
+    head: proxy.bind(app, "head"),
+    "delete": proxy.bind(app, "delete"),
+    options: proxy.bind(app, "options"),
+    trace: proxy.bind(app, "trace")
+  };
 
   function proxy(method, priority, path, ctrl) {
     if (! (method in router))
-      throw Error("method must be one of the following (get, post, put, head, delete, options, trace).");
+      throw new Error("method must be one of the following (all, get, post, put, head, delete, options, trace).");
     if (typeof priority !== "number")
-      throw TypeError("priority must be number");
+      throw new TypeError("priority must be number");
     if (typeof path !== "string")
-      throw TypeError("path must be string");
+      throw new TypeError("path must be string");
     if (typeof ctrl !== "function")
-      throw TypeError("ctrl must be function");
+      throw new TypeError("ctrl must be function");
 
     routes.push({
       primary: priority,
