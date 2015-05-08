77315dddd11ad65561deb0da33e928467e5b9c47
Takenori Nakagawa <ww24gm@gmail.com>
2015-05-09 01:52:56 +0900

    Update registration page

--- routes/new.js
+++ routes/new.js
@@ -4,82 +4,67 @@
 
 var express = require("express");
 var router = express.Router();
-var libs = require("../libs");
 var models = require("../models");
 
 router.post("/", function (req, res) {
   // check XHR
   if (! req.xhr) {
-    return res.json(400, {message: "Bad Request"});
+    return res.status(400).json({
+      status: "ng",
+      message: "Bad Request"
+    });
   }
 
   // check logged in
-  if (! req.session.profile)
-    return res.json(401, {message: "Unauthorized"});
-  if (req.session.status !== "new")
-    return res.json(403, {message: "Forbidden"});
+  if (! req.session.profile) {
+    return res.status(401).json({message: "Unauthorized"});
+  }
+  if (req.session.status !== "new") {
+    return res.status(403).json({message: "Forbidden"});
+  }
 
   var user = JSON.parse(JSON.stringify(req.session.profile));
   user.email = req.body.email;
   user.team = req.body.team || [];
 
   // check team property
-  if (typeof user.team === "string")
-    user.team = [req.body.team];
-  if (user.team.length === 0)
-    return res.json(400, {message: "Error", errors: ["チームを一つ以上選択してください"]});
+  if (! Array.isArray(user.team) || user.team.length === 0) {
+    return res.status(400).json({
+      status: "ng",
+      message: "Error",
+      errors: ["チームを一つ以上選択してください"]
+    });
+  }
 
-  models.Team.getNameList(function (err, teams) {
-    if (err) {
-      libs.logger.error(err);
-    }
+  var queries = user.team.map(function (team) {
+    return {name: team};
+  });
 
-    res.locals.teams = teams;
+  new Promise(function (resolve, reject) {
+    models.Team.find()
+    .or(queries)
+    .exec().then(resolve, reject);
+  }).then(function (teams) {
+    user.team = teams;
 
-    models.Role.findOne({name: "member"}, function (err, role) {
-      if (err) {
-        libs.logger.error(err);
-      }
+    return models.Role.findOne({name: "member"})
+    .exec();
+  }).then(function (role) {
+    user.role = role;
 
-      user.role = role;
+    return models.User.create(user);
+  }).then(function (user) {
+    req.session.status = null;
+    req.session.passport.user = user.id;
 
-      var queries = user.team.map(function (team) {
-        return {name: team};
-      });
-
-      models.Team.find()
-      .or(queries)
-      .exec(function (err, team) {
-        if (err) {
-          libs.logger.error(err);
-        }
-
-        user.team = team;
-
-        user = new models.User(user);
-        user.save(function (err) {
-          if (err) {
-            var errors = [];
-            if (err.errors) {
-              errors = Object.keys(err.errors).map(function (key) {
-                return key + ":" + err.errors[key].message;
-              });
-            } else if (err.err) {
-              errors.push("次の情報を管理者へお伝え下さい", "DB Error: " + err.err);
-            } else {
-              errors.push("次の情報を管理者へお伝え下さい", "DB Error: Unknown");
-            }
-
-            res.json(400, {message: "Error", errors: errors});
-            return libs.logger.error(err);
-          }
-
-          req.session.status = null;
-          req.session.passport.user = user.id;
-
-          res.json({status: "ok"});
-        });
-      });
+    res.json({
+      status: "ok",
+      user: user
+    });
+  }).catch(function (err) {
+    res.status(500).json({
+      status: "ng",
+      errors: [err.message]
     });
   });
 });
