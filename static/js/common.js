d1532bcf59af02d51c13fb03c64151b8b51a0422
Takenori Nakagawa <ww24gm@gmail.com>
2015-05-10 02:42:45 +0900

    Update style

--- static/js/common.js
+++ static/js/common.js
@@ -9,3 +9,8 @@ Vue.config.delimiters = ["[[", "]]"];
 window.addEventListener("error", function (err) {
   console.error(err);
 });
+
+$(function () {
+  // initialize navbar collapse
+  $("nav .button-collapse").sideNav();
+});
